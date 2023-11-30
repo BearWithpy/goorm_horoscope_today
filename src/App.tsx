73375b63a7ee0d853bb9773043805e5867751bbe
HermannChoi <hiyunseok347@gmail.com>
2023-11-30 21:35:00 +0900

    [EDIT]CardAndEtc

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
-import React from "react"
-import logo from "./logo.svg"
-import "./App.css"
-import Header from "./components/Header/Header"
-import CardBack from "./components/Card/CardBack";
-import CardFront from "./components/Card/CardFront";
+import React from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Card from "./components/Card/Card";
 import Footer from "./components/Footer/Footer";
 
 function App() {
-	return (
-		<div>
+  return (
+    <div>
       <Header />
-			<CardBack />
-			<CardFront />
-			<Footer />
-		</div>
-	)
+      <Card />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Card/CardBack.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./card.module.css";
-
-const CardBack = () => {
-	return <div className={styles.pattern}></div>;
-};
-
-export default CardBack;
