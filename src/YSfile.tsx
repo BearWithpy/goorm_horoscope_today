6c029fbc4718ff3dd6d20e06a02a26b46c885070
Junsu Park <pksjmh5295@gmail.com>
2023-11-30 14:16:33 +0900

    [REMOVE] Delete Demo files

--- src/YSfile.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function YSfile() {
-  return (
-    <div>YSfile part4</div>
-  )
-}
-
-export default YSfile
--- src/sooyeon/Sooyeon.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Sooyeon = () => {
-	return <div>happy happy happy 오후 12:52 깃 연습</div>;
-};
-
-export default Sooyeon;
