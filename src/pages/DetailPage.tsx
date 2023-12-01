c8f10ac1ec7d0c851da0680c7914cc2e8b272be2
Matilda007 <matilda230730@gmail.com>
2023-12-01 14:49:40 +0900

    [edit] detail page edit

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,14 +1,27 @@
 import React from "react";
+import styles from "pages/detailPage.module.css";
+import FrontCard from "components/Card/FrontCard";
 
 const DetailPage = () => {
-  const horo = {
-    fortune: "오늘은 행운과 긍정적인 에너지가 넘치는 날이다.",
-    quote: "할 수 있는 것을 하자. 불가능한 일을 기대하지 말자.",
-  };
+	const horo = {
+		fortune: "오늘은 행운과 긍정적인 에너지가 넘치는 날이다.",
+		quote: "할 수 있는 것을 하자. 불가능한 일을 기대하지 말자.",
+	};
 
-  // 운세: 오늘은 행운의 한 날이며, 사소한 일들에서도 큰 기쁨을 느낄 수 있는 날입니다.
-  // 명언: "행운은 그에게 고요한 사람에게 더 큰 호의를 베풀어 줍니다." - 에즈라 타운젠드
-  return <div></div>;
+	return (
+		<div className={styles.container}>
+			<FrontCard />
+
+			<div className={styles.fortune_quote_container}>
+				<div className={styles.fortune}>
+					<h1>{horo.fortune}</h1>
+				</div>
+				<div className={styles.quote}>
+					<h2>{horo.quote}</h2>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default DetailPage;
