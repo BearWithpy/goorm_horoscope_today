8dcf53b51efc72f31d676b51d7ea4f6b597fec3b
Junsu Park <pksjmh5295@gmail.com>
2023-12-01 10:48:01 +0900

    [INIT] Prettier Config

--- src/components/DemoComponent.tsx
+++ src/components/DemoComponent.tsx
@@ -1,7 +1,7 @@
-import React from "react"
+import React from "react";
 
 const DemoComponent = () => {
-    return <div></div>
-}
+  return <div></div>;
+};
 
-export default DemoComponent
+export default DemoComponent;
--- src/utils/horoscope.ts
+++ src/utils/horoscope.ts
@@ -1,21 +1,21 @@
-import OpenAI from "openai"
+import OpenAI from "openai";
 
-const apiKey = process.env.REACT_APP_OPENAI_API_KEY
-const openai = new OpenAI({ apiKey: apiKey, dangerouslyAllowBrowser: true })
+const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
+const openai = new OpenAI({ apiKey: apiKey, dangerouslyAllowBrowser: true });
 
 const horoscopeCall = async () => {
-    const completion = await openai.chat.completions.create({
-        messages: [
-            {
-                role: "system",
-                content:
-                    "오늘의 종합 운세를 한 문장으로 알려줘, 부정적인 운세는 없었으면 좋겠어. 그 후 이 운세에 어울리는 명언 하나도 알려줘",
-            },
-        ],
-        model: "gpt-3.5-turbo",
-    })
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content:
+          "오늘의 종합 운세를 한 문장으로 알려줘, 부정적인 운세는 없었으면 좋겠어. 그 후 이 운세에 어울리는 명언 하나도 알려줘",
+      },
+    ],
+    model: "gpt-3.5-turbo",
+  });
 
-    console.log(completion.choices[0].message.content)
-}
+  console.log(completion.choices[0].message.content);
+};
 
-export default horoscopeCall
+export default horoscopeCall;
