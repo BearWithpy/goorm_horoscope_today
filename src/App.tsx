4ebc39d2e4a140fd56669756c63fd63eae7c9bcd
Junsu Park <pksjmh5295@gmail.com>
2023-12-01 11:55:17 +0900

    [FEAT] parse to JSON - fortune text

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,29 @@ import horoscopeCall from "utils/horoscope";
 import DetailPage from "pages/DetailPage";
 
 const App = (): React.JSX.Element => {
-  // useEffect(() => {
-  //   horoscopeCall();
-  // }, []);
+  // async function fetchAndParseHoroscope() {
+  //   try {
+  //     const horoStr = await horoscopeCall();
+  //     if (horoStr) {
+  //       const horoJson = JSON.parse(horoStr);
+  //       return horoJson;
+  //     }
+  //     return null;
+  //   } catch (error) {
+  //     console.error("Horoscope fetching or parsing failed:", error);
+  //     return null;
+  //   }
+  // }
 
-  // example string
-  // 운세: 오늘은 행운의 한 날이며, 사소한 일들에서도 큰 기쁨을 느낄 수 있는 날입니다.
-  // 명언: "행운은 그에게 고요한 사람에게 더 큰 호의를 베풀어 줍니다." - 에즈라 타운젠드
+  // useEffect(() => {
+  //   fetchAndParseHoroscope().then((horoJson) => {
+  //     if (horoJson) {
+  //       console.log(horoJson);
+  //     } else {
+  //       console.log("No horoscope data received.");
+  //     }
+  //   });
+  // }, []);
 
   return (
     <div>
--- src/components/DemoComponent.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const DemoComponent = (): React.JSX.Element => {
-  return <div></div>;
-};
-
-export default DemoComponent;
