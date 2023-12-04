0f0a2765079ec5945dcc2a06356c38146b7a4c4c
Matilda007 <matilda230730@gmail.com>
2023-12-04 12:32:21 +0900

    [edit] z-index, DetailCard

--- src/components/Card/DetailCard.tsx
+++ src/components/Card/DetailCard.tsx
@@ -1,86 +1,73 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import styles from "components/Card/detailcard.module.css";
 
 const DetailCard = () => {
-  // 이미지 경로들을 저장할 상태
-  const [randomImagePaths, setRandomImagePaths] = useState<string[]>([]);
-  const [isVisible, setIsVisible] = useState(true);
+	// 이미지 경로들을 저장할 상태
+	const [randomImagePaths, setRandomImagePaths] = useState<string[]>([]);
+	const [isVisible, setIsVisible] = useState(true);
 
-  const imageUrls: string[] = [
-    "https://blog.kakaocdn.net/dn/bLY3BJ/btsBb8VXuP7/W8tkLTVdr688IL5Vq4S4VK/img.png",
-    "https://blog.kakaocdn.net/dn/d8tJmb/btsBfI9CyaT/QmUjMmgSm2TyPJlZbtTs1K/img.png",
-    "https://blog.kakaocdn.net/dn/bF9z7k/btsA7s2mZcQ/kW7Tbv3llKfKtEX8aLKufK/img.png",
-  ];
+	const imageUrls: string[] = [
+		"https://blog.kakaocdn.net/dn/bLY3BJ/btsBb8VXuP7/W8tkLTVdr688IL5Vq4S4VK/img.png",
+		"https://blog.kakaocdn.net/dn/d8tJmb/btsBfI9CyaT/QmUjMmgSm2TyPJlZbtTs1K/img.png",
+		"https://blog.kakaocdn.net/dn/bF9z7k/btsA7s2mZcQ/kW7Tbv3llKfKtEX8aLKufK/img.png",
+	];
 
-  // 컴포넌트가 마운트될 때 랜덤 이미지 경로를 선택
-  useEffect(() => {
-    setRandomImagePaths(getRandomImagePaths(imageUrls, 1));
-  }, []); // 빈 의존성 배열을 사용하여 처음 마운트될 때만 실행
+	// 컴포넌트가 마운트될 때 랜덤 이미지 경로를 선택
+	useEffect(() => {
+		setRandomImagePaths(getRandomImagePaths(imageUrls, 1));
+	}, []); // 빈 의존성 배열을 사용하여 처음 마운트될 때만 실행
 
-  //랜덤하게 카드를 뽑아준다.
-  function getRandomImagePaths(paths: string[], count: number): string[] {
-    const selectedImages = new Set<string>();
-    while (selectedImages.size < count) {
-      const randomIndex = Math.floor(Math.random() * paths.length);
-      selectedImages.add(paths[randomIndex]);
-    }
-    return Array.from(selectedImages);
-  }
+	//랜덤하게 카드를 뽑아준다.
+	function getRandomImagePaths(paths: string[], count: number): string[] {
+		const selectedImages = new Set<string>();
+		while (selectedImages.size < count) {
+			const randomIndex = Math.floor(Math.random() * paths.length);
+			selectedImages.add(paths[randomIndex]);
+		}
+		return Array.from(selectedImages);
+	}
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setRandomImagePaths(getRandomImagePaths(imageUrls, 1));
-    }, 3000);
-    return () => clearTimeout(timer);
-  }, []);
+	// useEffect(() => {
+	//   const timer = setTimeout(() => {
+	//     setRandomImagePaths(getRandomImagePaths(imageUrls, 1));
+	//   }, 3000);
+	//   return () => clearTimeout(timer);
+	// }, []);
 
-  useEffect(() => {
-    // 2초 후에 isVisible 상태를 false로 변경
-    const timer = setTimeout(() => {
-      setIsVisible(false);
-    }, 4000);
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setIsVisible(false);
+		}, 4000);
 
-    // 컴포넌트가 unmount 되면 타이머를 클리어
-    return () => clearTimeout(timer);
-  }, []); // 빈 배열을 전달하여 컴포넌트가 처음 마운트될 때만 실행
+		// 컴포넌트가 unmount 되면 타이머를 클리어
+		return () => clearTimeout(timer);
+	}, []); // 빈 배열을 전달하여 컴포넌트가 처음 마운트될 때만 실행
 
-  return (
-    <div className={styles.card_space}>
-      <div className={styles.flip}>
-        <div className={styles.card}>
-          <div className={styles.card_front}>
-            {randomImagePaths.map((path, index) => (
-              <img
-                className={styles.card_img}
-                key={index}
-                src={path}
-                alt={`Random Card Front ${index}`}
-              />
-            ))}
-          </div>
-          <div className={styles.card_back}>
-            <div className={styles.pattern}></div>
-          </div>
-        </div>
-      </div>
-      <div
-        className={styles.pattern}
-        style={{ display: isVisible ? "block" : "none" }}
-      ></div>
-      <div
-        className={styles.pattern}
-        style={{ display: isVisible ? "block" : "none" }}
-      ></div>
-      <div
-        className={styles.pattern}
-        style={{ display: isVisible ? "block" : "none" }}
-      ></div>
-      <div
-        className={styles.pattern}
-        style={{ display: isVisible ? "block" : "none" }}
-      ></div>
-    </div>
-  );
+	return (
+		<div className={styles.card_space}>
+			<div className={styles.flip}>
+				<div className={styles.card}>
+					<div className={styles.card_front}>
+						{randomImagePaths.map((path, index) => (
+							<img
+								className={styles.card_img}
+								key={index}
+								src={path}
+								alt={`Random Card Front ${index}`}
+							/>
+						))}
+					</div>
+					<div className={styles.card_back}>
+						<div className={styles.pattern}></div>
+					</div>
+				</div>
+			</div>
+			<div className={styles.pattern} style={{ display: isVisible ? "block" : "none" }}></div>
+			<div className={styles.pattern} style={{ display: isVisible ? "block" : "none" }}></div>
+			<div className={styles.pattern} style={{ display: isVisible ? "block" : "none" }}></div>
+			<div className={styles.pattern} style={{ display: isVisible ? "block" : "none" }}></div>
+		</div>
+	);
 };
 
 export default DetailCard;
